2e971fd47ddc8505c65985ccd5f185e2b3bacbfc
Richard Traindl <richardtraindl@gmail.com>
2019-12-19 14:31:30 +0100

    Update schema.py

--- ordi/schema.py
+++ ordi/schema.py
@@ -13,9 +13,9 @@ sqldrops = (
 
 
 sqlcreates = (
-    """CREATE TABLE auth_user (id SERIAL PRIMARY KEY, \
-        username VARCHAR(50) NOT NULL, \
-        password VARCHAR(256) NOT NULL, UNIQUE(username));""",
+    """CREATE TABLE auth_user (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
+        username VARCHAR(50) NOT NULL UNIQUE, \
+        password VARCHAR(256) NOT NULL);""",
     """CREATE TABLE person (id SERIAL PRIMARY KEY, \
        anredeartcode INTEGER NOT NULL, \
        titel VARCHAR(40), \
@@ -23,17 +23,17 @@ sqlcreates = (
        vorname VARCHAR(40), \
        notiz VARCHAR(200), \
        kunde BOOLEAN NOT NULL DEFAULT true);""",
-    """CREATE TABLE adresse (id SERIAL PRIMARY KEY, \
+    """CREATE TABLE adresse (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
        person_id INTEGER REFERENCES person(id) ON DELETE CASCADE, \
        strasse VARCHAR(40), \
        postleitzahl VARCHAR(10), \
        ort VARCHAR(40));""",
-    """CREATE TABLE kontakt (id SERIAL PRIMARY KEY, \
+    """CREATE TABLE kontakt (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
        person_id INTEGER REFERENCES person(id) ON DELETE CASCADE, \
        kontaktartcode INTEGER NOT NULL, \
        kontakt VARCHAR(50), \
        kontakt_intern VARCHAR(50));""",
-    """CREATE TABLE tier (id SERIAL PRIMARY KEY, \
+    """CREATE TABLE tier (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
        tiername VARCHAR(30), \
        tierart VARCHAR(30), \
        rasse VARCHAR(30), \
@@ -45,7 +45,7 @@ sqlcreates = (
        chip_nummer VARCHAR(30), \
        eu_passnummer VARCHAR(30), \
        patient BOOLEAN NOT NULL DEFAULT true);""",
-    """CREATE TABLE behandlung (id SERIAL PRIMARY KEY, \
+    """CREATE TABLE behandlung (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
        tier_id INTEGER REFERENCES tier(id) ON DELETE CASCADE, \
        behandlungsdatum TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, \
        gewicht_Kg VARCHAR(50), \
@@ -55,20 +55,20 @@ sqlcreates = (
        arzneien VARCHAR(255), \
        arzneimittel VARCHAR(100), \
        impfungen_extern VARCHAR(100));""",
-    """CREATE TABLE impfung (id SERIAL PRIMARY KEY, \
+    """CREATE TABLE impfung (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
         behandlung_id INTEGER REFERENCES beahndlung(id) ON DELETE CASCADE, \
         impfungsartcode INTEGER NOT NULL);""",
-    """CREATE TABLE behandlungsverlauf (id SERIAL PRIMARY KEY, \
+    """CREATE TABLE behandlungsverlauf (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
        person_id INTEGER REFERENCES person(id) ON DELETE CASCADE, \
        tier_id INTEGER REFERENCES tier(id) ON DELETE CASCADE, \
        datum TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, \
        diagnose VARCHAR(255), \
        behandlung VARCHAR(1000));""")
-    """CREATE TABLE tierhaltung (id SERIAL PRIMARY KEY, \
+    """CREATE TABLE tierhaltung (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
        person_id INTEGER REFERENCES person(id) ON DELETE CASCADE, \
        tier_id INTEGER REFERENCES tier(id) ON DELETE CASCADE, \
        anlagedatum TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP);""")
-    """CREATE TABLE rechnung (id SERIAL PRIMARY KEY, \
+    """CREATE TABLE rechnung (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
        person_id INTEGER REFERENCES person(id) ON DELETE CASCADE, \
        tier_id INTEGER REFERENCES tier(id) ON DELETE CASCADE, \
        rechnungjahr INTEGER NOT NULL, \
@@ -82,7 +82,7 @@ sqlcreates = (
        steuerbetrag_zwanzig DECIMAL(10,2) NOT NULL DEFAULT 0, \
        steuerbetrag_dreizehn DECIMAL(10,2) NOT NULL DEFAULT 0, \
        steuerbetrag_zehn DECIMAL(10,2) NOT NULL DEFAULT 0);""")
-    """CREATE TABLE rechnungzeile (id SERIAL PRIMARY KEY, \
+    """CREATE TABLE rechnungzeile (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
        rechnung_id INTEGER REFERENCES rechnung(id) ON DELETE CASCADE, \
        artikelartcode INTEGER NOT NULL, \
        datum DATE NOT NULL DEFAULT NOW, \

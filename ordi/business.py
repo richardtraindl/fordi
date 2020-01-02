a6c916d156cc004a6b6b8889677b0175a976ea27
Richard Traindl <richardtraindl@gmail.com>
2020-01-02 14:34:54 +0100

    Update business.py

--- ordi/business.py
+++ ordi/business.py
@@ -1,57 +1,25 @@
-Public Function calc_Rechnung(ByVal RechnungID As Long, ByRef Brutto_Summe, ByRef Netto_Summe, ByRef Steuerbetrag_Zwanzig, ByRef Steuerbetrag_Dreizehn, ByRef Steuerbetrag_Zehn)
-    Dim dbs As DAO.Database
-    Dim rs As DAO.Recordset
-    Dim colArtikelarten As Collection
-    Dim aktsteuersatz As Single
-    Dim aktnettobetrag As Single
-    
 
-    On Error GoTo calc_Rechnung_error
+from . import values.py
 
-    Set colArtikelarten = New Collection
-    Set dbs = OpenDatabase(CurrentDb.Name)
-    Set rs = dbs.OpenRecordset("SELECT * FROM tblArtikelart ORDER BY ID;")
-    rs.MoveFirst
-    Do While Not rs.EOF
-        colArtikelarten.Add (rs!Steuersatz)
-        rs.MoveNext
-    Loop
-    rs.Close
-    Set rs = Nothing
 
-    Brutto_Summe = 0
-    Netto_Summe = 0
-    Steuerbetrag_Zwanzig = 0
-    Steuerbetrag_Dreizehn = 0
-    Steuerbetrag_Zehn = 0
-    Set rs = dbs.OpenRecordset("SELECT * FROM tblRechnungzeile WHERE FK_RechnungID = " & RechnungID & ";")
-    rs.MoveFirst
-    Do While Not rs.EOF
-        Brutto_Summe = Brutto_Summe + rs!Betrag
-        ' sKey = Str(rs!FK_ArtikelartID)
-        aktsteuersatz = colArtikelarten.Item(rs!FK_ArtikelartID)
-        aktnettobetrag = rs!Betrag * 100 / (100 + aktsteuersatz)
-        If (aktsteuersatz = 20) Then
-            Steuerbetrag_Zwanzig = Steuerbetrag_Zwanzig + rs!Betrag - aktnettobetrag
-        ElseIf (aktsteuersatz = 13) Then
-            Steuerbetrag_Dreizehn = Steuerbetrag_Dreizehn + rs!Betrag - aktnettobetrag
-        ElseIf (aktsteuersatz = 10) Then
-            Steuerbetrag_Zehn = Steuerbetrag_Zehn + rs!Betrag - aktnettobetrag
-        End If
-        rs.MoveNext
-    Loop
-    
-    Steuerbetrag_Zwanzig = Round(Steuerbetrag_Zwanzig, 2)
-    Steuerbetrag_Dreizehn = Round(Steuerbetrag_Dreizehn, 2)
-    Steuerbetrag_Zehn = Round(Steuerbetrag_Zehn, 2)
-    Netto_Summe = Brutto_Summe - (Steuerbetrag_Zwanzig + Steuerbetrag_Dreizehn + Steuerbetrag_Zehn)
+def calc_rechnung(rechnungzeilen)
+    brutto_summe = 0
+    netto_summe = 0
+    steuerbetrag_zwanzig = 0
+    steuerbetrag_dreizehn = 0
+    steuerbetrag_zehn = 0
 
-    rs.Close
-    Set rs = Nothing
-    dbs.Close
-    Set dbs = Nothing
-    Exit Function
+    for rechnungzeile in rechnungzeilen:
+        brutto_summe += rechnungzeile.betrag
+        steuersatz = ARTIKEL_STEUER[rechnungzeile.artikelartcode]
+        nettobetrag += rechnungzeile.betrag * 100 / (100 + steuersatz)
+        if(steuersatz == 20):
+            steuerbetrag_zwanzig += (rechnungzeile.betrag - nettobetrag)
+        elif(steuersatz == 13):
+            steuerbetrag_dreizehn += (rechnungzeile.betrag - nettobetrag)
+        else: # steuersatz == 10
+            steuerbetrag_zehn += (rechnungzeile.betrag - nettobetrag)
 
-calc_Rechnung_error:
-    Set dbs = Nothing
-End Function
+    netto_summe = brutto_summe - (steuerbetrag_zwanzig + steuerbetrag_dreizehn + steuerbetrag_zehn)
+    return round(brutto_summe, 2), round(netto_summe, 2), round(steuerbetrag_zwanzig, 2), round(steuerbetrag_dreizehn, 2), round(steuerbetrag_zehn, 2)
+

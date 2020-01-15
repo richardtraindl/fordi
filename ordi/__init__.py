a284a4d1798b9d263f3a0e40457eaf76d31c7b4e
Richard Traindl <richardtraindl@gmail.com>
2020-01-15 16:05:14 +0100

    Update __init__.py

--- ordi/__init__.py
+++ ordi/__init__.py
@@ -10,6 +10,8 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'ordi.sqlite'),
+        UPLOAD_FOLDER = 'c:\temp\\ordi' # '/home/richard/ordi'
+        WKHTMLTOPDF_FOLDER = 'c:\\eprog\\wkhtmltopdf\\bin\\wkhtmltopdf.exe' # ''
     )
 
     if test_config is None:

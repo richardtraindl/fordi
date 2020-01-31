aa178996041bc9d2f8f8a64deb81bd2d676e2894
Richard Traindl <richardtraindl@gmail.com>
2020-01-31 16:46:45 +0100

    Update auth.py

--- ordi/auth.py
+++ ordi/auth.py
@@ -6,7 +6,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from ordi.db import get_db
+from ordi.db2 import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 

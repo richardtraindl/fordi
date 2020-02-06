9ef31d94f5f9db0e8c3b26b60fc6e1dc6ebb71e4
richardtraindl <richardtraindl@gmail.com>
2020-02-06 08:09:53 +0100

    impr.

--- ordi/__init__.py
+++ ordi/__init__.py
@@ -3,20 +3,20 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#from flask_sqlalchemy.engine import Engine
-#from flask_sqlalchemy import event
 from flask_migrate import Migrate
 
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 db = SQLAlchemy()
 migrate = Migrate()
-
-#@event.listens_for(Engine, "connect")
-#def set_sqlite_pragma(dbapi_connection, connection_record):
-#    cursor = dbapi_connection.cursor()
-#    cursor.execute("PRAGMA foreign_keys=ON")
-#    cursor.close()
-
 
 def create_app(test_config=None):
     # create and configure the app
